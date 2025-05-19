dc6d7f57d062e9084d4868fa4aa56753de6c2ce6
José Alberto <89354031+josealbertoraios13@users.noreply.github.com>
2025-05-19 12:47:25 -0300

    Add files via upload

---
+++ Semana3/Semana3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            string input = string.Empty;
+
+            string[] allowedOperation = { "+", "-", "*", "/" };
+
+            Console.WriteLine("== CALCULADORA BÁSICA ==");
+            Console.WriteLine("Digite uma operação!");
+            Console.WriteLine("adição          +");
+            Console.WriteLine("subtração       -");
+            Console.WriteLine("multiplicação   *");
+            Console.WriteLine("divisão         /");
+            Console.Write(">>> ");
+
+            var operation = Console.ReadLine() ?? "";
+
+            if (!IsValidOperation(allowedOperation, operation))
+                break;
+
+            Console.Write("\nDigite o primeiro número: ");
+
+            input = Console.ReadLine() ?? "";
+            var firstNumber = IsValidNumber(input);
+
+            if (firstNumber == null)
+                break;
+
+            Console.Write("\nDigite o segundo número: ");
+
+            input = Console.ReadLine() ?? "";
+            var secondNumber = IsValidNumber(input);
+
+            if (secondNumber == null)
+                break;
+
+            // ← AQUI VOCÊ INSERE o switch-case e completa a lógica
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine($"\nResultado: {firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+                    break;
+                case "-":
+                    Console.WriteLine($"\nResultado: {firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+                    break;
+                case "*":
+                    Console.WriteLine($"\nResultado: {firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+                    break;
+                case "/":
+                    if (secondNumber == 0)
+                    {
+                        Console.WriteLine("\nErro: divisão por zero não é permitida.Encerrando.");
+                        break;
+                    }
+
+                    Console.WriteLine($"\nResultado: {firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+                    break;
+            }
+
+            Console.Write("\nDeseja realizar mais alguma conta? Se sim digite 's' ");
+            input = Console.ReadLine() ?? "";
+
+            if (input != "s")
+                break;
+        }
+    }
+
+    private static bool IsValidOperation(string[] listOfOperations , string operation)
+    {
+        if (Array.IndexOf(listOfOperations, operation) < 0)
+        {
+            Console.WriteLine("Operação inválida, reinicie o programa");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static double? IsValidNumber(string input)
+    {
+        if (!double.TryParse(input, out double convertedNumber))
+        {
+            Console.WriteLine("Número inválido. Encerrando.");
+            return null;
+        }
+
+        return convertedNumber;
+    }
+}
